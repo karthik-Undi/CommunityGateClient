b9916ad8e5841fae65ce909ab694c55dca39607c
Karthik <karthik.venkata@kanini.com>
2021-03-31 15:45:00 +0530

    Add project files.

---
+++ CommunityGateClient/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommunityGateClient.Models.ViewModels;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CommunityGateClient.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginDetails loginDetails)
+        {
+            //string token;
+            using (var httpClient = new HttpClient())
+            {
+                ViewBag.message = "";
+
+                StringContent content = new StringContent(JsonConvert.SerializeObject(loginDetails), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("https://localhost:44388/api/Login/AuthenicateUser", content))
+                {
+
+
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                    {
+                        ViewBag.message = "Invalid User";
+
+                    }
+                    else
+                    {
+                        ViewBag.message = "Success";
+                    }
+                }
+
+            }
+            return View();
+
+
+
+        }
+
+    }
+}
---
+++ CommunityGateClient/Models/ViewModels/LoginDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunityGateClient.Models.ViewModels
+{
+    public class LoginDetails
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string LoginType { get; set; }
+    }
+}
