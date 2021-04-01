e4d77b66e0194be1e0b330364a481bbcda53cd92
Alint.Antony <Alint.Antony@KANINI-LTP-327>
2021-04-01 13:17:31 +0530

    Added Drop Down And Try And Catch And Other STuff

--- CommunityGateClient/Controllers/RegisterController.cs
+++ CommunityGateClient/Controllers/RegisterController.cs
@@ -1,10 +1,13 @@
-﻿using CommunityGateClient.Models.ViewModels;
+﻿using CommunityGateClient.Models;
+using CommunityGateClient.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,45 +16,114 @@ namespace CommunityGateClient.Controllers
     public class RegisterController : Controller
     {
         static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(RegisterController));
+        string BaseurlForHouseAPI = "http://localhost:26408/";
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult RegisterForResident()
+
+        [HttpGet]
+        public async Task<IActionResult> RegisterForResident()
         {
             _log4net.Info("Register For Resident Was Called");
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> RegisterForResident(RegisterDetailsForResident regDetailsResident)
-        {
-            if (ModelState.IsValid)
+            try
             {
-                //string token;
-                using (var httpClient = new HttpClient())
+                List<HouseList> freeHouseInfo = new List<HouseList>();
+                using (var client = new HttpClient())
                 {
-                    StringContent content = new StringContent(JsonConvert.SerializeObject(regDetailsResident), Encoding.UTF8, "application/json");
+                    client.BaseAddress = new Uri(BaseurlForHouseAPI);
 
-                    using (var response = await httpClient.PostAsync("http://localhost:27414/api/Resident", content))
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage Res = await client.GetAsync("api/House");
+
+                    if (Res.IsSuccessStatusCode)
                     {
-                        if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
-                        {
-                            _log4net.Info("Register Was Done With Email " + regDetailsResident.ResidentEmail + " But Registration Didnt Happen !!");
-                            ViewBag.message = "Registration Failed";
+                        var Response = Res.Content.ReadAsStringAsync().Result;
 
-                        }
-                        else
-                        {
-                            _log4net.Info("Register Was Done With Email " + regDetailsResident.ResidentEmail + " And the Registration Was Successfull !!");
-
-                            ViewBag.message = "Registered Successfully";
-                        }
+                        freeHouseInfo = JsonConvert.DeserializeObject<List<HouseList>>(Response);
+                        ViewBag.freeHouseDropDownList = new SelectList(freeHouseInfo, "HouseId", "HouseId");
+                    }
+                    else
+                    {
+                        ViewBag.freeHouseDropDownList = null;
                     }
 
                 }
+   
+            }
+            catch(Exception)
+            {
+                ViewBag.Message = "House API Not Reachable. Please Try Again Later.";
             }
             return View();
         }
+
+            [HttpPost]
+            public async Task<IActionResult> RegisterForResident(RegisterDetailsForResident regDetailsResident)
+            {
+            try
+            {
+                List<HouseList> freeHouseInfo = new List<HouseList>();
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(BaseurlForHouseAPI);
+
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage Res = await client.GetAsync("api/House");
+
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        var Response = Res.Content.ReadAsStringAsync().Result;
+
+                        freeHouseInfo = JsonConvert.DeserializeObject<List<HouseList>>(Response);
+                        ViewBag.freeHouseDropDownList = new SelectList(freeHouseInfo, "HouseId", "HouseId");
+                    }
+                    else
+                    {
+                        ViewBag.freeHouseDropDownList = null;
+                    }
+                }
+            }
+            catch(Exception)
+            {
+                ViewBag.Message = "House API Not Reachable. Please Try Again Later.";
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (var httpClient = new HttpClient())
+                    {
+                        StringContent content = new StringContent(JsonConvert.SerializeObject(regDetailsResident), Encoding.UTF8, "application/json");
+
+                        using (var response = await httpClient.PostAsync("http://localhost:27414/api/Resident", content))
+                        {
+                            if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                            {
+                                _log4net.Info("Registration Was Done With Email " + regDetailsResident.ResidentEmail + " But Registration Wasn't Successfull  !!");
+                                ViewBag.message = "Registration Failed";
+
+                            }
+                            else
+                            {
+                                _log4net.Info("Registration Was Done With Email " + regDetailsResident.ResidentEmail + " And the Registration Was Successfull !!");
+                                return RedirectToAction("Login", "Login");
+                            }
+                        }
+
+                    }
+                }
+                catch(Exception)
+                {
+                    ViewBag.Message = "Resident API Not Reachable. Please Try Again Later.";
+                }
+                }
+                
+
+                return View();
+            }
+        }
     }
-}
