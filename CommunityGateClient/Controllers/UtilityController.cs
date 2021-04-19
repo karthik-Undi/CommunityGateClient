bb40c8d51295c0ec7781be82483741164c25f364
Alint.Antony <Alint.Antony@KANINI-LTP-327>
2021-04-19 12:33:50 +0530

    Removed disabled and added condition  in utility wallet balance

--- CommunityGateClient/Controllers/UtilityController.cs
+++ CommunityGateClient/Controllers/UtilityController.cs
@@ -356,33 +356,38 @@ namespace CommunityGateClient.Controllers
                 {
                     ViewBag.Message = "Employee API Not Reachable. Please Try Again Later.";
                 }
-
-                    employees.EmployeeWallet = employees.EmployeeWallet - e.EmployeeWallet;
-                    try
+            if (e.EmployeeWallet <= employee.EmployeeWallet)
+            {
+                employees.EmployeeWallet = employees.EmployeeWallet - e.EmployeeWallet;
+                try
+                {
+                    using (var clientToUpdateEmployeeWallet = new HttpClient())
                     {
-                        using (var clientToUpdateEmployeeWallet = new HttpClient())
+                        clientToUpdateEmployeeWallet.BaseAddress = new Uri(baseUrlForEmployeeAPI);
+                        StringContent content = new StringContent(JsonConvert.SerializeObject(employees), Encoding.UTF8, "application/json");
+                        clientToUpdateEmployeeWallet.DefaultRequestHeaders.Clear();
+                        clientToUpdateEmployeeWallet.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        HttpResponseMessage Res = await clientToUpdateEmployeeWallet.PostAsync("api/Employees/UpdateEmployeeWallet/" + UserID, content);
+                        if (Res.StatusCode == System.Net.HttpStatusCode.BadRequest)
                         {
-                            clientToUpdateEmployeeWallet.BaseAddress = new Uri(baseUrlForEmployeeAPI);
-                            StringContent content = new StringContent(JsonConvert.SerializeObject(employees), Encoding.UTF8, "application/json");
-                            clientToUpdateEmployeeWallet.DefaultRequestHeaders.Clear();
-                            clientToUpdateEmployeeWallet.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                            HttpResponseMessage Res = await clientToUpdateEmployeeWallet.PostAsync("api/Employees/UpdateEmployeeWallet/" + UserID, content);
-                            if (Res.StatusCode == System.Net.HttpStatusCode.BadRequest)
-                            {
-                                ViewBag.Message = "Failed";
-                            }
-                            else
-                            {
-                                _log4net.Info("Wallet of Employee With ID " + UserID + " Was Updated !!");
+                            ViewBag.Message = "Failed";
+                        }
+                        else
+                        {
+                            _log4net.Info("Wallet of Employee With ID " + UserID + " Was Updated !!");
 
-                            }
                         }
                     }
-                    catch (Exception)
-                    {
-                        ViewBag.Message = "Employee API Not Reachable. Please Try Again Later.";
-                    }
-
+                }
+                catch (Exception)
+                {
+                    ViewBag.Message = "Employee API Not Reachable. Please Try Again Later.";
+                }
+            }
+            else
+            {
+                ViewBag.Message = "The wallet balance is empty.";
+            }
 
                 return View(employees);
         }
